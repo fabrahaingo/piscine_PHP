0d3f615a1867d0320532db1c3eac4939c92dc473
Fabien RAHAINGOMANANA <frahaing@e2r5p2.42.fr>
2018-03-13 14:00:40 +0100

    Fixing do_op_2

--- j01/ex11/do_op_2.php
+++ j01/ex11/do_op_2.php
@@ -1,48 +1,25 @@
 #!/usr/bin/php
 <?php
 
-function is_well_formated($string) {
-    $sign = 0;
-    $step = 0;
-    for ($i = 0; is_numeric($string[$i]); $i++);
-    for ($i; !is_numeric($string[$i]); $i++) {
-        if (($string[$i] == '+' || $string[$i] == '-' || $string[$i] == '*' || $string[$i] == '/' || $string[$i] == '%') && $sign == 0)
-            $sign = 1;
-        else if (($string[$i] == '+' || $string[$i] == '-' || $string[$i] == '*' || $string[$i] == '/' || $string[$i] == '%') && $sign != 0)
-            return (false);
-        else if ($string[$i] == ' ')
-            continue;
-        else
-            return (false);
-    }
-    for ($i; is_numeric($string[$i]); $i++);
-    return (true);
-}
-
-function get_delim($string) {
-    for ($i = 0; is_numeric($string[$i]); $i++);
-    return ($string[$i]);
-}
-
 if ($argc < 2) {
     echo "Incorrect Parameters\n";
     return ;
 }
-unset($argv[0]);
-foreach ($argv as $av) {
-    $full_input .= $av;
-}
-$full_input = trim($full_input);
-if (is_well_formated($full_input)) {
-    str_replace(" ", "", $full_input);
-    $delim = get_delim($full_input);
-    for ($i = 0; is_numeric($full_input[$i]); $i++)
-        $num1 .= $full_input[$i];
-    $i += 1;
-    for ($i; is_numeric($full_input[$i]); $i++)
-        $num2 .= $full_input[$i];
-    echo $num1 {$delim} $num2;
-}
+$str = str_replace("\n", "", $argv[1]);
+$num1 = intval($str);
+print($num1);
+$delim = $str[strlen($num1)];
+$num2 = substr($str, (strlen($num1)));
+if ($delim == '+')
+    echo $num1 + $num2;
+else if ($delim == '-')
+    echo $num1 - $num2;
+else if ($delim == '*')
+    echo $num1 * $num2;
+else if ($delim == '/')
+    echo $num1 / $num2;
+else if ($delim == '%')
+    echo $num1 % $num2;
 else
     echo "Syntax Error\n"
 
