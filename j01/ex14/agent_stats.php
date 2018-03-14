482f2946be9c0b38944fc3f1cd07d8856c21ffd4
Fabien RAHAINGOMANANA <frahaing@e3r2p19.42.fr>
2018-03-14 16:56:44 +0100

    ex14 finally done

--- j01/ex14/agent_stats.php
+++ j01/ex14/agent_stats.php
@@ -16,36 +16,85 @@ function fill_tab($string) {
 
 function sort_alpha($category, $tab) {
     for ($i = 1; $tab[$i]; $i++) {
-        print("coucou");
-        if ($tab[$i + 1] && (strcmp($tab[$i][$category], $tab[$i + 1][$category]) < 0)) {
+        if (!$tab[$i + 1])
+            return ($tab);
+        if (strcmp($tab[$i][$category], $tab[$i + 1][$category]) > 0) {
             $tmp = $tab[$i];
             $tab[$i] = $tab[$i + 1];
             $tab[$i + 1] = $tmp;
             $i = 0;
-            print('moved ' . $y++ . " times\n");
         }
     }
 }
 
 $stdin = fopen("php://stdin", "r");
 $tab = fill_tab($stdin);
-sort_alpha($user, $tab);
-print_r($tab[1]);
-#foreach($tab as $t)
-#    print_r($t);
 if ($argc == 2) {
     if ($argv[1] == "moyenne") {
-        while ($line = fgets($stdin)) {
-            $tab = explode (";", $line);
-            if (is_numeric($tab[1])) {
-                $total += $tab[1];
+        foreach ($tab as $t) {
+            if (is_numeric($t[note])) {
+                $total += $t[note];
                 $nb_grades++;
             }
         }
         $result = $total / $nb_grades;
+        if ($result != NULL)
+
+            echo $result . "\n";
     }
-    if ($result != NULL) {
-        echo $result . "\n";
+    else if ($argv[1] == "moyenne_user") {
+        $tab = sort_alpha("user", $tab);
+        for ($i = 0; $tab[$i]; $i++) {
+            while ($tab[$i][user] == $tab[$i + 1][user]) {
+                if (is_numeric($tab[$i][note])) {
+                    $total += $tab[$i][note];
+                    $nb_grades++;
+                }
+                $i++;
+            }
+            if (is_numeric($tab[$i][note])) {
+                $total += $tab[$i][note];
+                $nb_grades++;
+            }
+            if ($nb_grades != NULL) {
+                $result = $total / $nb_grades;
+                echo $tab[$i][user] . ":" . $result . "\n";
+            }
+            $total = 0;
+            $nb_grades = 0;
+        }
+    }
+    else if ($argv[1] == "ecart_moulinette") {
+        $tab = sort_alpha("user", $tab);
+        for ($i = 0; $tab[$i]; $i++) {
+            while ($tab[$i][user] == $tab[$i + 1][user]) {
+                if (is_numeric($tab[$i][note]) && $tab[$i][noteur] != "moulinette") {
+                    $total += $tab[$i][note];
+                    $nb_diff++;
+                }
+                else if (is_numeric($tab[$i][note]) && $tab[$i][noteur] == "moulinette") {
+                    $total_moul += $tab[$i][note];
+                    $nb_moul++;
+                }
+                $i++;
+            }
+            if (is_numeric($tab[$i][note]) && $tab[$i][noteur] != "moulinette") {
+                $total += $tab[$i][note];
+                $nb_diff++;
+            }
+            else if (is_numeric($tab[$i][note]) && $tab[$i][noteur] == "moulinette") {
+                $total_moul += $tab[$i][note];
+                $nb_moul++;
+            }
+            if ($nb_diff != NULL && $nb_moul != NULL) {
+                $result = ($total / $nb_diff) - ($total_moul / $nb_moul);
+                echo $tab[$i][user] . ":" . $result . "\n";
+            }
+            $total = 0;
+            $nb_diff = 0;
+            $total_moul = 0;
+            $nb_moul = 0;
+        }
     }
 }
 ?>
