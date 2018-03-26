64bb3ebfc81b53d5790ba2e73a061b50ce8cc83d
Fabien RAHAINGOMANANA <frahaing@e3r1p13.42.fr>
2018-03-26 23:08:27 +0200

    j02: ex03 - almost there

--- j02/ex03/who.php
+++ j02/ex03/who.php
@@ -1,1 +1,17 @@
- 
+#!/usr/bin/php
+<?php
+    date_default_timezone_set("Europe/Paris");
+
+    $file = file_get_contents("/var/run/utmpx");
+    $format = "a256user/a4id/a32line/ipid/itype/I2time/a256host/i16pad";
+    $file = substr($file, 628);
+    while (($file = substr($file, 628)) != NULL)
+    {
+        $tab = unpack($format, $file);
+        echo $tab[user]." ";
+        echo $tab[line]." ";
+        echo date("M d H:i", $tab[time1]) ."\n";
+    }
+    print_r($tab);
+
+?>
