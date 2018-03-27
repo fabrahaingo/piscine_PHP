cd40ca07bb168d0c33037e20c1c6ae5a0bd8e267
Fabien RAHAINGOMANANA <frahaing@e3r2p18.42.fr>
2018-03-28 01:03:27 +0200

    Added chat file and iframes

--- j04/ex04/chat.php
+++ j04/ex04/chat.php
@@ -1,12 +1,19 @@
 <?php
 
-
+session_start();
+if (!($_SESSION['loggued_on_user'])) {
+    header('Refresh: 2; URL="index.html"');
+    echo "You are not currently connected. Please log in.\n";
+}
+else {
+    if (file_exists('../private/chat')) {
+        $full_conversation = unserialize(file_get_contents('../private/chat'));
+        foreach ($full_conversation as $message) {
+            echo $message['user'] . " ";
+            echo "(" . date($message['time']) . "): ";
+            echo $message['msg'] . "\n";
+        }
+    }
+}
 
 ?>
-<html>
-<head>
-    <meta charset='utf-8' />
-    <style>
-    </style>
-</head>
-<body>
--- j04/ex04/speak.php
+++ j04/ex04/speak.php
@@ -1,12 +1,55 @@
 <?php
 
-
-
+session_start();
+if (!($_SESSION['loggued_on_user'])) {
+    header('Refresh: 2; URL="index.html"');
+    echo "You are not currently connected. Please log in.\n";
+}
+else {
+    if ($_POST['new_message']) {
+        if (!file_exists("../private/chat")) {
+            mkdir("../private");
+            file_put_contents("../private/chat", null);
+        }
+        $full_conversation = unserialize(file_get_contents("../private/chat"));
+        $fd = fopen("../private/chat", 'w');
+        flock($fd, LOCK_EX);
+        $message['login'] = $_SESSION['loggued_on_user'];
+        $message['time'] = time();
+        $message['msg'] = $_POST['new_message'];
+        $full_conversation[] = $message;
+        file_put_contents("../private/chat", serialize($full_conversation));
+        fclose($fd);
+    }
 ?>
 <html>
 <head>
     <meta charset='utf-8' />
     <style>
+
+    .chat_field {
+        width: 90%;
+        height: 33px;
+        padding: 0;
+    }
+
+    .button {
+        width: 9%;
+        border: none;
+        padding: 9.5px;
+        margin-left: 8px;
+    }
+
     </style>
+    <script langage="javascript">top.frames['chat'].location = 'chat.php';</script>
 </head>
 <body>
+<form action='speak.php' method='POST'>
+    <input class="chat_field" type='text' name="new_message" value='' />
+    <input class="button" type='submit' name='submit' value='Send' />
+</form>
+</body>
+</html>
+<?php
+}
+?>
