277e292d8d019e6a2c5e1b302a7d42228ec0a9aa
Fabien RAHAINGOMANANA <frahaing@e3r2p18.42.fr>
2018-03-27 19:57:56 +0200

    modif singit

--- j01/ex13/sing_it!.php
+++ j01/ex13/sing_it!.php
@@ -4,13 +4,7 @@
 if ($argc == 2) {
     if ($argv[1] == "mais pourquoi cette demo ?") {
         echo "Tout simplement pour qu'en feuilletant le sujet\n";
-        echo "on ne s'apercoive pas de la nature de l'exo\n";
-    }
-    if ($argv[1] == "mais pourquoi cette chanson ?")
-        echo "Parce que Kwame a des enfants";
-    if ($argv[1] == "vraiment ?") {
-        $array = array ("Nan c'est parce que c est le premier avril\n", "Oui il a vraiment des enfants\n");
-        echo $array[array_rand($array, 1)];
+        echo "on ne s'apercoive pas de la nature de l'exo..\n";
     }
 }
 
