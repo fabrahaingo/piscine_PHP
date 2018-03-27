5d3303bb611b2ff0b09e4d127be00b97ddcd0814
Fabien RAHAINGOMANANA <frahaing@e3r2p18.42.fr>
2018-03-27 17:14:44 +0200

    fixed is_sort

--- j01/ex08/ft_is_sort.php
+++ j01/ex08/ft_is_sort.php
@@ -3,7 +3,8 @@
 function ft_is_sort($tab) {
     $array1 = $tab;
     $array2 = $tab;
-    sort($array2);
+    if ($array2)
+        sort($array2);
     if ($array1 != $array2) {
         return (false);
     }
