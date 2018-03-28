14c98224e5c8f0fc2741cdb2333e63a60241c35d
Fabien RAHAINGOMANANA <frahaing@e3r2p18.42.fr>
2018-03-28 23:06:55 +0200

    Sorting each character after the other

--- j01/ex09/ssap2.php
+++ j01/ex09/ssap2.php
@@ -1,41 +1,58 @@
 #!/usr/bin/php
 <?php
 
-function filter($var){
+function filter($var) {
   return ($var !== NULL && $var !== FALSE && $var !== '');
 }
 
+function ft_swap(&$x, &$y) {
+    list($x, $y) = array($y, $x);
+}
+
+function is_sort($word1, $word2) {
+    $word1 = strtolower($word1);
+    $word2 = strtolower($word2);
+    $alphabet = array_filter(explode(" ", "a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9"), 'filter');
+    $length = strlen($word1) < strlen($word2) ? strlen($word1) : strlen($word2);
+    for ($i = 0; $i < $length; $i++)
+    {
+        $short_word1 = substr($word1, $i, 1);
+        $short_word2 = substr($word2, $i, 1);
+        $pos_word1 = array_search($short_word1, $alphabet);
+        $pos_word2 = array_search($short_word2, $alphabet);
+        if ($pos_word1 === false)
+            $pos_word1 = ord($short_word1) + 100;
+        else {}
+        if ($pos_word2 === false)
+            $pos_word2 = ord($short_word2) + 100;
+        else {}
+        if ($pos_word1 > $pos_word2)
+            return false;
+        if ($pos_word1 < $pos_word2)
+            return true;
+    }
+    if (strlen($word1) <= strlen($word2))
+        return true;
+    else
+        return false;
+    #return strlen($word1) <= strlen($word2) ? true : false;
+}
+
+$result = array();
+$i = 0;
 if ($argc > 1) {
     unset($argv[0]);
     foreach ($argv as $v) {
         $trimed = trim($v);
         $tab = array_filter(explode(' ', $trimed), 'filter');
-        foreach ($tab as $t) {
-            if (ord($t[0]) < 48 || (ord($t[0]) >= 58 && ord($t[0]) <= 64) ||
-            (ord($t[0]) >= 91 && ord($t[0]) <= 96) || ord($t[0] > 123))
-                $sp_char[] = $t;
-            else if (ord($t[0]) < 65) {
-                $num[] = $t;
-            }
-            else
-                $char[] = $t;
-        }
+        $result = array_merge($result, $tab);
     }
-    if ($char) {
-        natcasesort($char);
-        foreach ($char as $c)
-            echo $c . "\n";
-    }
-    if ($num) {
-        sort($num, SORT_STRING);
-        foreach ($num as $n)
-            echo $n . "\n";
-    }
-    if ($sp_char) {
-        sort($sp_char);
-        foreach ($sp_char as $sp)
-            echo $sp . "\n";
-    }
+    usort($result, "is_sort");
+    $i++;
+    $result = array_reverse($result);
+    foreach ($result as $word)
+        echo $word . "\n";
 }
+
 
 ?>
