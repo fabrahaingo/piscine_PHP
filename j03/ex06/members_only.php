26c79d6c14717a2608c4aa77e05682a2c3006cbb
Fabien RAHAINGOMANANA <frahaing@e3r2p17.42.fr>
2018-03-29 16:05:43 +0200

    some presentation fixes

--- j03/ex06/members_only.php
+++ j03/ex06/members_only.php
@@ -3,15 +3,15 @@
 $username = $_SERVER['PHP_AUTH_USER'];
 $password = $_SERVER['PHP_AUTH_PW'];
 if ($username != "zaz" || $password != "jaimelespetitsponeys") {
-    header('WWW-Authenticate: Basic realm=\'\'Espace membres\'\'');
     header('HTTP/1.1 401 Unauthorized');
+    header('WWW-Authenticate: Basic realm=\'\'Espace membres\'\'');
     echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>";
 }
 else {
     $img = base64_encode(file_get_contents('../../img/42.png'));
-?>
-<html><body>Bonjour Zaz<br /><img src='data:image/png;base64, \" . $img . \"'</img></body></html>
-<?php
+    echo "<html><body>
+Bonjour Zaz<br />
+<img src='data:image/png;base64,".$img."')</img></body></html>";
 }
 
 ?>
