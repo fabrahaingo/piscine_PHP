aa01ce0c245e30f42df1e04cfca0e74d7385cc6e
Fabien RAHAINGOMANANA <frahaing@e1r10p3.42.fr>
2018-03-29 18:07:11 +0200

    Added isset for ex03 & presentation fixes for ex05

--- j03/ex03/cookie_crisp.php
+++ j03/ex03/cookie_crisp.php
@@ -1,13 +1,16 @@
 <?php
 
-if ($_GET['action'] == "set" && $_GET['name'] && $_GET['value']) {
+if ($_GET['action'] == "set" && $_GET['name'] && isset($_GET['value'])) {
     setcookie($_GET['name'], $_GET['value'], time() + 3600);
 }
 if ($_GET['action'] == "del" && $_GET["name"]) {
     setcookie($_GET['name'], "", time() - 3600);
 }
-if ($_GET['action'] == "get" && $_COOKIE[$_GET['name']] && $_GET['name']) {
-    echo $_COOKIE[$_GET['name']] . "\n";
+if ($_GET['action'] == "get" && isset($_GET['name'])) {
+    foreach ($_COOKIE as $cook => $value) {
+        if ($cook == $_GET['name'])
+            echo $_COOKIE[$_GET['name']] . "\n";
+    }
 }
 
 ?>
--- j03/ex06/members_only.php
+++ j03/ex06/members_only.php
@@ -1,17 +1,23 @@
 <?php
-
-$username = $_SERVER['PHP_AUTH_USER'];
-$password = $_SERVER['PHP_AUTH_PW'];
-if ($username != "zaz" || $password != "jaimelespetitsponeys") {
-    header('HTTP/1.1 401 Unauthorized');
-    header('WWW-Authenticate: Basic realm=\'\'Espace membres\'\'');
-    echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>";
+if ($_SERVER["PHP_AUTH_USER"] == "zaz" && $_SERVER["PHP_AUTH_PW"] == "jaimelespetitsponeys")
+{
+?>
+<html><body>
+Bonjour Zaz<br />
+<img src='data:image/png;base64,<?php
+$img = file_get_contents("../img/42.png");
+$img = base64_encode($img);
+echo $img;
+?>
+'>
+</body></html>
+<?php
 }
 else {
-    $img = base64_encode(file_get_contents('../../img/42.png'));
-    echo "<html><body>
-Bonjour Zaz<br />
-<img src='data:image/png;base64,".$img."')</img></body></html>";
+    header("WWW-Authenticate: Basic realm=''Espace membres''");
+    header("HTTP/1.0 401 Unauthorized");
+?>
+<html><body>Cette zone est accessible uniquement aux membres du site</body></html>
+<?php
 }
-
 ?>
