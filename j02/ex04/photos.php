75517f4f9882ce2453df234e1de3d7967da55ae2
Fabien RAHAINGOMANANA <frahaing@e1r10p3.42.fr>
2018-03-29 23:40:04 +0200

    modified

--- j02/ex04/photos.php
+++ j02/ex04/photos.php
@@ -1,56 +1,49 @@
 #!/usr/bin/php
 <?php
-
-function getFile($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $tmp = curl_exec($ch);
-    curl_close($ch);
-    if ($tmp != false){
-        return $tmp;
-    }
-}
-
-function getDirName($fullName) {
-    if (preg_match('#https?://(.*)#', $fullName, $tab))
-        return ($tab[1]);
-    else
-        return ($fullName);
-}
-
-function getFileName($url) {
-    $file = strrchr($url, '/');
-    $file = substr($file, 1);
-    return ($file);
-}
-
-$init = curl_init($argv[1]);
-curl_setopt($init, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($init, CURLOPT_BINARYTRANSFER, true);
-$code = curl_exec($init);
-curl_close($init);
-$there = preg_match_all('#<img.*?>#is', $code, $array);
-if ($there != FALSE) {
-    $dir_name = getDirName($argv[1]);
-    if (file_exists($dir_name)) {
-        $files = glob("./" . $dir_name . "/*");
-        foreach ($files as $name)
-            unlink($name);
-        rmdir($dir_name);
-    }
-    mkdir($dir_name . "/");
-    foreach($array[0] as $ar) {
-        if (preg_match('#src="(https?://)(.*?)"#i', $ar, $tab)) {
-            $url = $tab[2];
+    if ($argc == 2)
+    {
+        $c = curl_init();
+        curl_setopt($c, CURLOPT_URL, $argv[1]);
+        curl_setopt($c, CURLOPT_HEADER, false);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        if ($str = curl_exec($c))
+        {
+            $contentType = curl_getinfo($c, CURLINFO_CONTENT_TYPE);
+            if (!preg_match("/https?:\/\/([^\/]+)/", $argv[1], $f))
+                return;
+            echo $f[1] . "\n";
+            if (!is_dir($f[1] . "/"))
+                if (!mkdir($f[1] . "/"))
+                    return;
+            if (preg_match("/^image/", $contentType))
+            {
+                preg_match("/(?:.*\/)?([^?]*)/", $argv[1], $name);
+                file_put_contents($f[1] . "/" . $name[1], $str);
+                return;
+            }
+            preg_match_all("/<img .*?src=[\"']?([^ '\">]*)/", $str, $i);
+            foreach ($i[1] as $link)
+            {
+                if (!preg_match("/^https?:\/\//", $link))
+                {
+                    if ($link[0] === '/' && $argv[1][strlen($argv[1]) - 1] !== '/')
+                        $link = $argv[1] . $link;
+                    elseif ($link[0] === '/' && $argv[1][strlen($argv[1]) - 1] === '/')
+                        $link = $argv[1] . substr($link, 1);
+                    elseif ($link[0] !== '/' && $argv[1][strlen($argv[1]) - 1] === '/')
+                        $link = $argv[1] . $link;
+                    elseif ($link[0] !== '/' && $argv[1][strlen($argv[1]) - 1] !== '/')
+                        $link = $argv[1] . "/" . $link;
+                }
+                curl_setopt($c, CURLOPT_URL, $link);
+                curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+                if ($img = curl_exec($c))
+                {
+                    preg_match("/(?:.*\/)?([^?]*)/", $link, $name);
+                    file_put_contents($f[1] . "/" . $name[1], $img);
+                }
+            }
+            curl_close($c);
         }
-        else if (preg_match('#src="(.*?)"#i', $ar, $tab)) {
-            $url = $argv[1] . $tab[1];
-        }
-        $file = getFileName($url);
-        file_put_contents($dir_name . "/" . $file, getFile($url));
     }
-}
-
 ?>
