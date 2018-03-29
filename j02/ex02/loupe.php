365c77e8b5b5fbcd8cb1bb955a46c13a3ebbca09
Fabien RAHAINGOMANANA <frahaing@e1r10p3.42.fr>
2018-03-29 21:39:35 +0200

    fixed <article>

--- j02/ex02/loupe.php
+++ j02/ex02/loupe.php
@@ -6,14 +6,14 @@ function upper($matches) {
 }
 
 function ft_in_link($match) {
-    $match[0] = preg_replace_callback('#(")(.*?)(")#msi', 'upper', $match[0]);
-    $match[0] = preg_replace_callback('#(>)(.*?)(<)#msi', 'upper', $match[0]);
+    $match[0] = preg_replace_callback('#(title=")(.*?)(")#smi', 'upper', $match[0]);
+    $match[0] = preg_replace_callback('#(>)(.*?)(<)#smi', 'upper', $match[0]);
     return ($match[0]);
 }
 if ($argc > 1 && filesize($argv[1]) > 0) {
     $rawfile = fopen($argv[1], "r");
     $file = fread($rawfile, filesize($argv[1]));
-    $file = preg_replace_callback('#<a.*>.*</a>#msi', 'ft_in_link', $file);
+    $file = preg_replace_callback('#<a[ | .*?>].*?</a>#msi', 'ft_in_link', $file);
     echo $file;
     fclose($rawfile);
 }
